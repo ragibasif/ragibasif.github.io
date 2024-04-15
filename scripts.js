ec73b3df53f1b92936f4bfe277f37bcb2214bc89
Ragib Asif <>
2024-04-15 00:43:09 -0400

    add some styling

--- scripts.js
+++ scripts.js
@@ -1,2 +1,27 @@
-// scripts.js
-// Add your JavaScript code here
+// Dark Mode
+document.addEventListener("DOMContentLoaded", () => {
+  const darkModeToggle = document.getElementById("darkModeToggle");
+
+  darkModeToggle.addEventListener("click", () => {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+  });
+
+  // Sticky Navbar
+  const navbar = document.getElementsByClassName("navbar");
+
+  if (navbar) {
+    const sticky = navbar.offsetTop;
+
+    function stickyNavbar() {
+      if (window.scrollY >= sticky) {
+        navbar.classList.add("sticky");
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+  }
+  window.onscroll = function () {
+    stickyNavbar();
+  };
+});
