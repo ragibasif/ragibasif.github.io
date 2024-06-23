fef00bd0f4d572392243ee176e34962f817efb6e
Ragib Asif <github.decibel284@passinbox.com>
2024-06-23 18:39:17 -0400

    add hacked text effect to h1

--- scripts.js
+++ scripts.js
@@ -85,6 +85,7 @@ mail.addEventListener("mouseleave", () => {
 const currentYear = new Date().getFullYear();
 document.getElementById("copyright-year").textContent = currentYear;
 
+// effect for my name
 // magic star
 let index = 0,
   interval = 1000;
@@ -107,3 +108,32 @@ for (const star of document.getElementsByClassName("magic-star")) {
     setInterval(() => animate(star), 1000);
   }, index++ * (interval / 3));
 }
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let freq = null;
+
+document.getElementById("role").onmouseover = (event) => {
+  let iteration = 0;
+
+  clearInterval(freq);
+
+  freq = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(freq);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+};
