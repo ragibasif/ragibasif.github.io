55fd1370ba7b01e3707054d31be0232ca631ba0e
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2024-11-03 21:52:24 -0500

    wip

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("copyright-year").textContent = currentYear;
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
