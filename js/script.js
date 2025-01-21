3763650b58067c37149c1186940e6cc22b4e7d7b
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-01-20 19:41:06 -0500

    MAJOR REFACTOR

--- js/script.js
+++ js/script.js
@@ -1,42 +1,74 @@
-// Dynamically update the copyright year to be the current year.
-function renderCopyright() {
-  const copyrightOwner = "Ragib Asif";
-  const currentYear = new Date().getFullYear();
-  const copyrightContainer = document.getElementById("copyright");
-  const copyrightText = document.createElement("p");
-  copyrightText.innerHTML = `&copy; ${currentYear} ${copyrightOwner}. All rights reserved.`;
-  copyrightContainer.append(copyrightText);
+const NAME = "Ragib Asif";
+const YEAR = new Date().getFullYear();
+const CONTAINERS = {
+  title: "title-container",
+  header: "header-container",
+  nav: "navigation-container",
+  projects: "projects-container",
+  footer: "footer-container",
+};
+
+function renderLayout() {
+  renderTitle(NAME);
+  renderHeader(NAME);
+  renderNav();
+  renderProjects();
+  renderFooter();
 }
 
-function renderSocials() {
-  // Fetch the JSON file containing the socials information and links
-  fetch("../assets/data/socials.json")
-    .then((response) => response.json())
-    .then((data) => {
-      const socialsList = document.querySelector(".socials");
+function renderTitle(title) {
+  const titleContainer = document.getElementById(CONTAINERS.title);
+  titleContainer.innerText = title;
+}
 
-      // Populate the socials list
-      data.socials.forEach((social) => {
-        const list = document.createElement("li");
-        const link = document.createElement("a");
-        link.href = `${social.url}`;
-        link.target = "_blank";
-        link.rel = "noopener noreferrer";
-        link.innerHTML = `${social.name}`;
-        list.appendChild(link);
-        socialsList.appendChild(list);
-      });
-    })
-    .catch((error) => console.error("Error fetching socials:", error));
+function renderHeader(name) {
+  const headerContainer = document.getElementById(CONTAINERS.header);
+  const mainHeader = document.createElement("h1");
+  mainHeader.innerText = name;
+  headerContainer.appendChild(mainHeader);
+}
+
+function renderNav() {
+  const pageLinks = [
+    {
+      title: "Home",
+      id: CONTAINERS.header,
+    },
+    {
+      title: "Projects",
+      id: CONTAINERS.projects,
+    },
+    {
+      title: "Contact",
+      id: CONTAINERS.footer,
+    },
+  ];
+  const navigationContainer = document.getElementById(CONTAINERS.nav);
+  const allLinksList = document.createElement("ul");
+  pageLinks.forEach((link) => {
+    const itemLi = document.createElement("li");
+    const itemA = document.createElement("a");
+    itemA.href = `#${link.id}`;
+    itemA.innerText = `${link.title}`;
+    itemLi.appendChild(itemA);
+    allLinksList.appendChild(itemLi);
+  });
+  navigationContainer.appendChild(allLinksList);
 }
 
 function renderProjects() {
+  // create header and list container
+  const projectsContainer = document.getElementById(CONTAINERS.projects);
+  const projectsHeader = document.createElement("h2");
+  projectsHeader.innerText = "Projects";
+  const projectsList = document.createElement("ul");
+  projectsList.classList.add("projects");
+  projectsContainer.appendChild(projectsHeader);
+  projectsContainer.appendChild(projectsList);
   // Fetch the JSON file with the projects information
   fetch("../assets/data/projects.json")
     .then((response) => response.json())
     .then((data) => {
-      const projectsList = document.querySelector(".projects");
-
       // Populate the projects list
       data.projects.forEach((project) => {
         const list = document.createElement("li");
@@ -70,6 +102,49 @@ function renderProjects() {
     .catch((error) => console.error("Error fetching projects:", error));
 }
 
-renderCopyright();
-renderSocials();
-renderProjects();
+// Dynamically update the copyright year to be the current year.
+function renderCopyright(name, year) {
+  const copyrightOwner = name;
+  const currentYear = year;
+  const copyrightContainer = document.getElementById("copyright");
+  const copyrightText = document.createElement("p");
+  copyrightText.innerHTML = `&copy; ${currentYear} ${copyrightOwner}. All rights reserved.`;
+  copyrightContainer.append(copyrightText);
+}
+
+function renderSocials() {
+  // Fetch the JSON file containing the socials information and links
+  fetch("../assets/data/socials.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const socialsList = document.querySelector(".socials");
+
+      // Populate the socials list
+      data.socials.forEach((social) => {
+        const list = document.createElement("li");
+        const link = document.createElement("a");
+        link.href = `${social.url}`;
+        link.target = "_blank";
+        link.rel = "noopener noreferrer";
+        link.innerHTML = `${social.name}`;
+        list.appendChild(link);
+        socialsList.appendChild(list);
+      });
+    })
+    .catch((error) => console.error("Error fetching socials:", error));
+}
+
+function renderFooter() {
+  const footerContainer = document.getElementById(CONTAINERS.footer);
+  const socials = document.createElement("ul");
+  socials.classList.add("socials");
+  const copyright = document.createElement("div");
+  copyright.id = "copyright";
+  footerContainer.appendChild(socials);
+  footerContainer.appendChild(copyright);
+
+  renderSocials();
+  renderCopyright(NAME, YEAR);
+}
+
+renderLayout();
