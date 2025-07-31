f024c0209799d9599babd9c6720e4ca863c1a459
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-07-31 14:52:38 -0400

    feat: add updated pages

--- src/consts.ts
+++ src/consts.ts
@@ -18,9 +18,20 @@ export const BLOG: Metadata = {
   DESCRIPTION: "A collection of articles on topics I am passionate about.",
 };
 
+export const NOTES: Metadata = {
+  TITLE: "Notes",
+  DESCRIPTION: "A collection of notes on topics I am studying.",
+};
+
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
   DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+};
+
+
+export const WORK: Metadata = {
+  TITLE: "Work",
+  DESCRIPTION: "My work history.",
 };
 
 export const SOCIALS: Socials = [
