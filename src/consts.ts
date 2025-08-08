7e0a8f42b98e031d84570de79165f16f6f87f6f7
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-08-08 17:05:12 -0400

    fix: remove unnecessary elements

--- src/consts.ts
+++ src/consts.ts
@@ -1,49 +1,22 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-import type { Site, Metadata, Socials } from "@types";
+import type { Site, Socials } from "src/types";
 
 export const SITE: Site = {
   NAME: "Ragib Asif",
-  EMAIL: "ragib.asif30@myhunter.cuny.edu",
-};
-
-export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "A minimal and lightweight blog and portfolio.",
-};
-
-export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
-};
-
-export const NOTES: Metadata = {
-  TITLE: "Notes",
-  DESCRIPTION: "A collection of notes on topics I am studying.",
-};
-
-export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
-};
-
-
-export const WORK: Metadata = {
-  TITLE: "Work",
-  DESCRIPTION: "My work history.",
 };
 
 export const SOCIALS: Socials = [
-  { 
+  {
     NAME: "GitHub",
     HREF: "https://github.com/ragibasif"
   },
-  { 
+  {
     NAME: "LinkedIn",
     HREF: "https://www.linkedin.com/in/ragibasif",
   },
-  { 
+  {
     NAME: "YouTube",
     HREF: "https://www.youtube.com/@ragib_asif",
   }
--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,5 @@
 export type Site = {
   NAME: string;
-  EMAIL: string;
-};
-
-export type Metadata = {
-  TITLE: string;
-  DESCRIPTION: string;
 };
 
 export type Socials = {
